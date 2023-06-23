fff1a198820bb3bfc8bfc3824ed461be68f434fa
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-06-22 21:18:19 -0300

    :recycle:	refactor: ajustando timer para ter apenas uma instancia

--- modulo4/jakartaee/src/main/java/io/github/andresgois/job/AgendamentoEmailJob.java
+++ modulo4/jakartaee/src/main/java/io/github/andresgois/job/AgendamentoEmailJob.java
@@ -3,14 +3,27 @@ package io.github.andresgois.job;
 import java.util.List;
 
 import javax.ejb.Schedule;
+import javax.ejb.Singleton;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import io.github.andresgois.entity.AgendamentoEmail;
 import io.github.andresgois.service.AgendamentoEmailServico;
 
-@Stateless
+//@Stateless
+@Singleton
 public class AgendamentoEmailJob {
+	
+	/*private static AgendamentoEmailJob instance;
+	
+	private AgendamentoEmailJob() {}
+	
+	public synchronized static AgendamentoEmailJob getInstance() {
+		if(instance == null) {
+			instance = new AgendamentoEmailJob();
+		}
+		return instance;
+	}*/
 
 	@Inject
 	private AgendamentoEmailServico agendamentoEmailServico;
