c5cd8cf55be09c509883563c0f8a9ef10098249b
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-06-23 21:22:06 -0300

    :sparkles:	feat: criando fila para envio de email

--- modulo4/jakartaee/src/main/java/io/github/andresgois/job/AgendamentoEmailJob.java
+++ modulo4/jakartaee/src/main/java/io/github/andresgois/job/AgendamentoEmailJob.java
@@ -2,10 +2,14 @@ package io.github.andresgois.job;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
 
 import io.github.andresgois.entity.AgendamentoEmail;
 import io.github.andresgois.service.AgendamentoEmailServico;
@@ -28,11 +32,28 @@ public class AgendamentoEmailJob {
 	@Inject
 	private AgendamentoEmailServico agendamentoEmailServico;
 	
+	/*
+	 *  ConnectionFactory para a fila
+	 *  fabrica de conexao
+	 */
+	@Inject
+	@JMSConnectionFactory("java:/jboss/DefaultJMSConnectionFactory")
+	private JMSContext context;
+	
+	/*
+	 * Fila
+	 */
+	@Resource(mappedName = "java:/jms/queue/EmailQueue")
+	private Queue queue;
+	
+	
 	@Schedule(hour = "*", minute = "*", second = "*/10")
 	public void enviarEmail() {
 		List<AgendamentoEmail> lista =  agendamentoEmailServico.listarPorNaoAgendado();
 		lista.forEach(e -> {
-			agendamentoEmailServico.enviar(e);
+			//agendamentoEmailServico.enviar(e);
+			// envia pra fila
+			context.createProducer().send(queue, e);
 			agendamentoEmailServico.alterar(e);
 		});
 	}
