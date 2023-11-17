f393f5c18e58d1f278a3317c5fdff03fd3434adc
andre <andresousalee@gmail.com>
2023-11-16 21:30:47 -0300

    ✨feature: Integrando a tela de promoções com Websocket

--- Java_EE_Projeto_Final/menssageria/casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Promo.java
+++ Java_EE_Projeto_Final/menssageria/casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Promo.java
@@ -1,5 +1,9 @@
 package br.com.casadocodigo.loja.models;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+
 public class Promo {
 
     private String titulo;
@@ -20,4 +24,12 @@ public class Promo {
     public void setLivro(Livro livro) {
         this.livro = livro;
     }
+
+	public String toJson() {
+		JsonObjectBuilder promo = Json.createObjectBuilder();
+		promo.add("titulo", titulo)
+			.add("livroId", livro.getId());
+		
+		return promo.build().toString();
+	}
 }
---
+++ Java_EE_Projeto_Final/menssageria/casadocodigo/src/main/java/br/com/casadocodigo/loja/websockets/UsuariosSession.java
@@ -0,0 +1,21 @@
+package br.com.casadocodigo.loja.websockets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.Session;
+
+@ApplicationScoped // vai garantir que esse usuário exista por todo o ciclo de vida dessa aplicação
+public class UsuariosSession {
+
+	private List<Session> sessions =  new ArrayList<>();
+	
+	public void add(Session session) {
+		sessions.add(session);
+	}
+	
+	public List<Session> getUsuarios(){
+		return sessions;
+	}
+}
